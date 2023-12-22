7440733bdc659d30c1d35891c597b8b58aa4c2a5
Narendra <narendrach34@gmail.com>
2023-12-22 12:17:50 +0530

    navbar-img

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,14 @@
 import React from "react";
 import Searchbox from "./searchBox.js";
 import Button from "./Button";
-import { ReactComponent as Logo } from "../assets/qtify-icon.svg";
 import styles from "./Header.module.css";
+import qtifyLogo from "../assets/qtify-logo.png";
 
 export default function Header() {
   return (
     <nav>
       <div className={styles.navContainer}>
-        <div className={styles.logo}>
-        <Logo />
-        </div>
+        <img src={qtifyLogo} alt="qtify logo" />
         <Searchbox placeholder="Search a album of your choice" />
         <Button>Give Feedback</Button>
       </div>
